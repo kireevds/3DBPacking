01522116d792c2000e22711092600ca50f944ab1
Dmitrii Kireev <kireevds@gmail.com>
2016-10-16 21:02:34 +0300

    ??????? ???? ?????????. ??????? ??? ??????

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,9 +6,59 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->containersTable->setRowCount(1);
+    ui->containersTable->setColumnCount(5);
+    ui->containersTable->setHorizontalHeaderLabels(QStringList() << "Тип" << "Ширина" << "Длина" << "Высота" << "Количество");
+
+    ui->objectsTable->setRowCount(1);
+    ui->objectsTable->setColumnCount(5);
+    ui->objectsTable->setHorizontalHeaderLabels(QStringList() << "Тип" << "Ширина" << "Длина" << "Высота" << "Количество");
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_newContainerButton_clicked()
+{
+    ui->containersTable->insertRow(ui->containersTable->rowCount());
+}
+
+void MainWindow::on_deleteContainerButton_clicked()
+{
+    ui->containersTable->removeRow(ui->containersTable->currentRow());
+}
+
+void MainWindow::on_newObjectButton_clicked()
+{
+    ui->objectsTable->insertRow(ui->objectsTable->rowCount());
+}
+
+void MainWindow::on_deleteObjectButton_clicked()
+{
+    ui->objectsTable->removeRow(ui->objectsTable->currentRow());
+}
+
+void MainWindow::on_mDataClear_triggered()
+{
+    ui->containersTable->clear();
+    ui->containersTable->setRowCount(1);
+    ui->objectsTable->clear();
+    ui->objectsTable->setRowCount(1);
+}
+
+void MainWindow::on_mExit_triggered()
+{
+    this->close();
+}
+
+void MainWindow::on_mDataLoad_triggered()
+{
+
+}
+
+void MainWindow::on_mDataSave_triggered()
+{
+
 }
--- mainwindow.h
+++ mainwindow.h
@@ -15,6 +15,23 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void on_newContainerButton_clicked(); //Добавить строку под контейнер
+
+    void on_newObjectButton_clicked(); //Добавить строку под объект
+
+    void on_deleteContainerButton_clicked(); //Удалить строку с данными о контейнере
+
+    void on_deleteObjectButton_clicked(); //Удалить строку с данными об объекте
+
+    void on_mDataClear_triggered(); //Очистить обе таблицы
+
+    void on_mExit_triggered(); //Меню - Выход
+
+    void on_mDataLoad_triggered(); //Меню - Загрузить данные
+
+    void on_mDataSave_triggered(); //Меню - Сохранить данные
+
 private:
     Ui::MainWindow *ui;
 };
